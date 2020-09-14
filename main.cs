5a3171d4fdc0e4cc1a823fec6fd147fd5f6ae702
rhuerta2220 <65692699+rhuerta2220@users.noreply.github.com>
2020-09-14 00:09:21 -0700

    Add files via upload

--- main.cs
+++ main.cs
@@ -1,19 +1,18 @@
-/// Lab No. 1		Exercise No. 1
-/// File Name: MyFirstProgram.cs
+
+/// Lab No. 2		Exercise No. 1
+/// File Name: Lab2.cs
 /// @author: Ricardo Huerta
 /// Date:  September 13, 2020
 ///
-/// Problem Statement: Ask the user to enter two numbers, calculate the sum of
-/// these two numbers, and display the sum to the screen
+/// Problem Statement: Write a program that defines a variable initially assigned to the number of coupons you win. 
 /// 
 /// 
 /// Overall Plan:
-/// 1) Print an initial welcoming message to the screen
-/// 2) Prompt the user for two integers
-/// 3) Calculate the sum of the integers
-/// 4) Print the sum of the integers to the screen
+/// 1) Prompt user to enter number of tickets they have
+/// 2) Calculate how many candy bars per 10 tickes. (priority to gumballs)
+/// 3) Calculate how many gumballs can be bought with remain tickets
+/// 4) Print results to user
 /// 
-
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -22,42 +21,19 @@ using System.Threading.Tasks;
 
 namespace ConsoleApplication1
 {
-    class MyFirstProgram
-    {
-        static void Main(string[] args)
-        {
-            // print a message to the screen
-            Console.WriteLine("Hello out there.");
-            Console.WriteLine("I will add two numbers for you.");
-            Console.WriteLine("Enter one number and press enter, then enter second number and press enter and enter third number:");
+  class MainClass {
+    public static void Main (string[] args) {
+      Console.WriteLine("How many ticket do you have?");
+      int tickets = Int32.Parse(Console.ReadLine());
+      int temp = tickets;
 
-            // declare two integer variables
-            int n1, n2,n3, sum, product;
-            double divide;
+      int candy = tickets/10;
+      tickets = tickets%10;
+      int gum = tickets/3;
 
-            string userInput;
-            userInput = Console.ReadLine();
-            n1 = Int32.Parse(userInput);
-            n2 = Int32.Parse(Console.ReadLine());
-            n3 = Int32.Parse(Console.ReadLine());
-
-            // calculate the sum of these two numbers
-            sum = n1 + n2+ n3;
-            product = n1 * n2 * n3;
-            divide = product/sum;
+      Console.WriteLine("You can get "+ candy + " Candy Bars and " + gum + " Gum ball with " + temp + " tickets.");
 
 
-            // print a message along with the sum of the two numbers
-            Console.WriteLine("The sum of those three numbers is");
-            Console.WriteLine(sum);
-            Console.WriteLine("The product of those three numbers is");
-            Console.WriteLine(product);
-            Console.WriteLine("The sum divide by those three numbers is");
-            Console.WriteLine(divide);
-
-            //Just to pause the screen.
-            Console.ReadLine();
-
-        }
     }
+  }
 }
